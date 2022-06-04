d7960a5ce54a4d6fa7dbba3913f57af57385547f
laurazp <laurikazp@gmail.com>
2022-06-04 22:42:42 +0200

    Project created. Tab Bar working

---
+++ Quake/Quake/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Quake
+//
+//  Created by laurazp on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
+            
+                getData(from: url)
+            }
+            
+            struct Response: Codable {
+                let type: String
+                let features: [Feature]
+                
+            }
+
+            struct Feature: Codable {
+                let properties: Property
+                let geometry: Geometry
+            }
+            
+            struct Property: Codable {
+                let mag: Double
+                let place: String
+                //let time: Date
+                let tsunami: Int
+                let title: String
+                let coordinates: [Int]
+            }
+            
+            struct Geometry: Codable {
+                let coordinates: [Float]
+            }
+
+            private func getData(from url: String) {
+                let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+                    guard let data = data, error == nil else {
+                        print("Something went wrong...")
+                        return
+                    }
+                    
+                    // Have data
+                    var result: Response?
+                    do {
+                        result = try JSONDecoder().decode(Response.self, from: data)
+                    }
+                    catch {
+                        print("Failed to convert \(error.localizedDescription)")
+                    }
+
+                    guard let json = result else {
+                        return
+                    }
+                    
+                    print(json.type)
+                    
+                })
+                    task.resume()
+            }
+
+}
+
