ee5e693b52d7f5d380afa6e6ce7b840382e5f905
laurazp <laurikazp@gmail.com>
2022-06-17 19:24:19 +0100

    Configuring MapKit

--- Quake/Quake/Map/MapViewController.swift
+++ Quake/Quake/Map/MapViewController.swift
@@ -1,18 +1,108 @@
-//
-//  MapViewController.swift
-//  Quake
-//
-//  Created by Guillermo Zafra on 14/6/22.
-//
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
 
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    private let mapView = MKMapView(frame: .zero)
+    private let locationManager = CLLocationManager()
+    private let rangeInMeters: Double = 10000
+     
     override func viewDidLoad() {
         super.viewDidLoad()
+                    
+        layoutUI()
+    }
+     
+    private func layoutUI() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mapView)
+            
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func checkLocationServices() {
+        guard CLLocationManager.locationServicesEnabled() else {
+            // Here we must tell user how to turn on location on device
+            return
+        }
+            
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        checkAuthorizationForLocation()
+    }
 
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+    private func checkAuthorizationForLocation() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            mapView.showsUserLocation = true
+            centerViewOnUser()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // Here we must tell user how to turn on location on device
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+                // Here we must tell user that the app is not authorize to use location services
+            break
+        @unknown default:
+            break
+        }
+    }
+
+    private func centerViewOnUser() {
+        guard let location = locationManager.location?.coordinate else { return }
+        
+        let coordinateRegion = MKCoordinateRegion.init(center: location,
+                                                       latitudinalMeters: rangeInMeters,
+                                                       longitudinalMeters: rangeInMeters)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    /*private func configureMapTypeButton() {
+        mapTypeButton.delegate = self
+     
+        mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "map")!, labelTitle: "Standard", action: {
+            self.mapView.mapType = .mutedStandard
+        })
+            
+       mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "satellite")!, labelTitle: "Satellite", action: {
+           self.mapView.mapType = .satellite
+       })
+            
+       mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "hybrid")!, labelTitle: "Hybrid", action: {
+           self.mapView.mapType = .hybrid
+       })
+            
+       mapTypeButton.setFABButton()
+    }*/
+    
+}
+
+
+
+extension MapViewController {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkAuthorizationForLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+                let coordinateRegion = MKCoordinateRegion.init(center: location.coordinate,
+                                                               latitudinalMeters: rangeInMeters,
+                                                               longitudinalMeters: rangeInMeters)
+                
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 }
