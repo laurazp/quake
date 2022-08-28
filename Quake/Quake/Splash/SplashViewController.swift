490824323bb7b042e0f62cca71f93316484dbfc1
laurazp <laurikazp@gmail.com>
2022-08-29 01:07:31 +0200

    Sound added to Splash animation

--- Quake/Quake/Splash/SplashViewController.swift
+++ Quake/Quake/Splash/SplashViewController.swift
@@ -1,11 +1,14 @@
 
 import UIKit
 import Lottie
+import AVFoundation
 
 class SplashViewController: UIViewController {
 
     @IBOutlet weak var animationView: AnimationView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,7 @@ class SplashViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        playSound(file: "logo_sound", ext: "mp3") //TODO: Cambiar por algún sonido !!!!!
         DispatchQueue.main.async {
             UIView.animate(withDuration: 1.5, animations: {
                 self.animationView.alpha = 1.0
@@ -47,5 +51,22 @@ class SplashViewController: UIViewController {
             })
         }
     }
+    
+    //TODO: Modificar con sonido concreto !!!!
+    private func playSound(file:String, ext:String) -> Void {
+        do {
+            let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: file, ofType: ext)!)
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        } catch let error {
+            NSLog(error.localizedDescription)
+        }
+    }
+    
+    //TODO: Copiar en info de la app!!!!! --> créditos
+    // Sound Effect by <a href="https://pixabay.com/users/muzaproduction-24990238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=13775">Muzaproduction</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=13775">Pixabay</a>
+    
+    
 }
     
