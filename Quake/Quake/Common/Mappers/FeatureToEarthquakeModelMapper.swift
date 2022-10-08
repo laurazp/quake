bb84209ad0eb4b308576df474b56b2b5c8a4adc4
laurazp <laurikazp@gmail.com>
2022-10-08 12:36:50 +0100

    Using Measurement to be able to change user Units later

--- Quake/Quake/Common/Mappers/FeatureToEarthquakeModelMapper.swift
+++ Quake/Quake/Common/Mappers/FeatureToEarthquakeModelMapper.swift
@@ -13,10 +13,17 @@ struct FeatureToEarthquakeModelMapper {
                         place: feature.properties.place ?? "Unknown",
                         formattedCoords: getFormattedCoordsFormatter.getFormattedCoords(actualCoords: feature.geometry.coordinates),
                         originalCoords: feature.geometry.coordinates,
-                        depth: "\(String(feature.geometry.coordinates[2]))km", //no debería ser opcional?
+                        depth: "\(Measurement(value: Double(feature.geometry.coordinates[2]).roundToDecimal(2), unit: UnitLength.kilometers))",
                         date: getDateFormatter.formatDate(dateToFormat: feature.properties.time ?? 0000),
                         originalDate: getDateFormatter.formatIntToDate(dateToFormat: feature.properties.time ?? 0),
                         tsunami: getTsunamiValueFormatter.getTsunamiValue(tsunami: feature.properties.tsunami ?? 0),
                         magnitude: String(format: "%.1f", feature.properties.mag ?? 0))
+    }
+}
+
+extension Double {
+    func roundToDecimal(_ fractionDigits: Int) -> Double {
+        let multiplier = pow(10, Double(fractionDigits))
+        return Darwin.round(self * multiplier) / multiplier
     }
 }
