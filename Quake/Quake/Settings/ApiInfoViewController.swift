b9e80fb5798f7f5496aa1c770eaf9f45a792c1db
laurazp <laurikazp@gmail.com>
2022-11-01 20:18:17 +0000

    API info updated

--- Quake/Quake/Settings/ApiInfoViewController.swift
+++ Quake/Quake/Settings/ApiInfoViewController.swift
@@ -9,7 +9,9 @@ class ApiInfoViewController: UIViewController {
     @IBOutlet weak var creditsTitleLabel: UILabel!
     @IBOutlet weak var creditsContentLabel: UILabel!
     @IBOutlet weak var developerTitleLabel: UILabel!
-    @IBOutlet weak var developerContentLabel: UILabel!
+    @IBOutlet weak var developerContentLabel: UILabel!    
+    @IBOutlet weak var credits2ContentLabel: UILabel!
+    @IBOutlet weak var credits3ContentLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,48 +28,77 @@ class ApiInfoViewController: UIViewController {
     }
     
     private func setupLabels() {
+        //API Info
         apiInfoTitleLabel.text = "API Info"
         
-        //Add link to apiInfoContentLabel
         var apiInfoTextArray = [String]()
         var apiInfoFontArray = [UIFont]()
         var apiInfoColorArray = [UIColor]()
         
         apiInfoTextArray.append("Data source credit belongs to")
         apiInfoTextArray.append("USGS (United States Geological Survey)")
-        //apiInfoTextArray.append("(United States Geological Survey)")
         
-        apiInfoFontArray.append(.systemFont(ofSize: 17))
         apiInfoFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
-        //apiInfoFontArray.append(.systemFont(ofSize: 17))
+        apiInfoFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
         
-        apiInfoColorArray.append(.black)
+        apiInfoColorArray.append(.systemGray)
         apiInfoColorArray.append(.systemBlue)
-        //apiInfoColorArray.append(.black)
 
         apiInfoContentLabel.attributedText = getAttributedString(arrayText: apiInfoTextArray, arrayColors: apiInfoColorArray, arrayFonts: apiInfoFontArray)
         
+        //Credits
         creditsTitleLabel.text = "\nCredits"
         
-        //Add link to creditsContentLabel
         var creditsTextArray = [String]()
         var creditsFontArray = [UIFont]()
         var creditsColorArray = [UIColor]()
         
         creditsTextArray.append("Seismic icons created by")
-        creditsTextArray.append("Freepik - Flaticon")
+        creditsTextArray.append("Freepik - Flaticon\n")
         
-        creditsFontArray.append(.systemFont(ofSize: 17))
+        creditsFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
         creditsFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
         
-        creditsColorArray.append(.black)
+        creditsColorArray.append(.systemGray)
         creditsColorArray.append(.systemBlue)
 
         creditsContentLabel.attributedText = getAttributedString(arrayText: creditsTextArray, arrayColors: creditsColorArray, arrayFonts: creditsFontArray)
-                
+        
+        //Credits2
+        var credits2TextArray = [String]()
+        var credits2FontArray = [UIFont]()
+        var credits2ColorArray = [UIColor]()
+        
+        credits2TextArray.append("Earthquake icons created by")
+        credits2TextArray.append("fjstudio - Flaticon\n")
+        
+        credits2FontArray.append(.systemFont(ofSize: 17, weight: .semibold))
+        credits2FontArray.append(.systemFont(ofSize: 17, weight: .semibold))
+        
+        credits2ColorArray.append(.systemGray)
+        credits2ColorArray.append(.systemBlue)
+
+        credits2ContentLabel.attributedText = getAttributedString(arrayText: credits2TextArray, arrayColors: credits2ColorArray, arrayFonts: credits2FontArray)
+        
+        //Credits3
+        var credits3TextArray = [String]()
+        var credits3FontArray = [UIFont]()
+        var credits3ColorArray = [UIColor]()
+        
+        credits3TextArray.append("API info links code from")
+        credits3TextArray.append("leoiphonedev - Github\n")
+        
+        credits3FontArray.append(.systemFont(ofSize: 17, weight: .semibold))
+        credits3FontArray.append(.systemFont(ofSize: 17, weight: .semibold))
+        
+        credits3ColorArray.append(.systemGray)
+        credits3ColorArray.append(.systemBlue)
+
+        credits3ContentLabel.attributedText = getAttributedString(arrayText: credits3TextArray, arrayColors: credits3ColorArray, arrayFonts: credits3FontArray)
+        
+        //Support
         developerTitleLabel.text = " \nSupport"
         
-        //Add link to developerContentLabel
         var developerTextArray = [String]()
         var developerFontArray = [UIFont]()
         var developerColorArray = [UIColor]()
@@ -75,10 +106,10 @@ class ApiInfoViewController: UIViewController {
         developerTextArray.append("If you have any questions, please feel free to contact us at")
         developerTextArray.append("quake@gmail.com")
         
-        developerFontArray.append(.systemFont(ofSize: 17))
+        developerFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
         developerFontArray.append(.systemFont(ofSize: 17, weight: .semibold))
         
-        developerColorArray.append(.black)
+        developerColorArray.append(.systemGray)
         developerColorArray.append(.systemBlue)
 
         developerContentLabel.attributedText = getAttributedString(arrayText: developerTextArray, arrayColors: developerColorArray, arrayFonts: developerFontArray)
@@ -94,6 +125,18 @@ class ApiInfoViewController: UIViewController {
         let creditsTapgesture = UITapGestureRecognizer(target: self, action: #selector(tappedOnLabel(_ :)))
         creditsTapgesture.numberOfTapsRequired = 1
         self.creditsContentLabel.addGestureRecognizer(creditsTapgesture)
+        
+        // Configure credits2ContentLabel for TapGesture
+        self.credits2ContentLabel.isUserInteractionEnabled = true
+        let credits2Tapgesture = UITapGestureRecognizer(target: self, action: #selector(tappedOnLabel(_ :)))
+        credits2Tapgesture.numberOfTapsRequired = 1
+        self.credits2ContentLabel.addGestureRecognizer(credits2Tapgesture)
+        
+        // Configure credits3ContentLabel for TapGesture
+        self.credits3ContentLabel.isUserInteractionEnabled = true
+        let credits3Tapgesture = UITapGestureRecognizer(target: self, action: #selector(tappedOnLabel(_ :)))
+        credits3Tapgesture.numberOfTapsRequired = 1
+        self.credits3ContentLabel.addGestureRecognizer(credits3Tapgesture)
         
         // Configure developerContentLabel for TapGesture
         self.developerContentLabel.isUserInteractionEnabled = true
@@ -131,6 +174,12 @@ class ApiInfoViewController: UIViewController {
         guard let creditsText = self.creditsContentLabel.text else { return }
         let creditsLinkRange = (creditsText as NSString).range(of: "Freepik - Flaticon")
         
+        guard let credits2Text = self.credits2ContentLabel.text else { return }
+        let credits2LinkRange = (credits2Text as NSString).range(of: "fjstudio - Flaticon")
+        
+        guard let credits3Text = self.credits3ContentLabel.text else { return }
+        let credits3LinkRange = (credits3Text as NSString).range(of: "leoiphonedev - Github")
+        
         guard let developerText = self.developerContentLabel.text else { return }
         let developerLinkRange = (developerText as NSString).range(of: "quake@gmail.com")
         
@@ -146,7 +195,6 @@ class ApiInfoViewController: UIViewController {
                 UIApplication.shared.openURL(url)
             }
         } else if gesture.didTapAttributedTextInLabel(label: self.creditsContentLabel, inRange: creditsLinkRange) {
-            //Open web page
             guard let url = URL(string: "https://www.flaticon.com/free-icons/seismic") else {
               return
             }
@@ -157,9 +205,27 @@ class ApiInfoViewController: UIViewController {
                 UIApplication.shared.openURL(url)
             }
             
+        } else if gesture.didTapAttributedTextInLabel(label: self.credits2ContentLabel, inRange: credits2LinkRange) {
+            guard let url = URL(string: "https://www.flaticon.com/free-icons/earthquake") else {
+              return
+            }
+
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
             
-            //https://github.com/leoiphonedev/DetectLinksUILabel --> link for Credits
-            
+        } else if gesture.didTapAttributedTextInLabel(label: self.credits3ContentLabel, inRange: credits3LinkRange) {
+            guard let url = URL(string: "https://github.com/leoiphonedev/DetectLinksUILabel") else {
+              return
+            }
+
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
             
         } else if gesture.didTapAttributedTextInLabel(label: self.developerContentLabel, inRange: developerLinkRange) {
             // Show alert before sending an e-mail
@@ -190,3 +256,4 @@ class ApiInfoViewController: UIViewController {
         }
     }
 }
+
